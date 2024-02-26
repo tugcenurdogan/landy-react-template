d5809c60665a978e1d2cf74f40c1ad3db64cafcf
Tugce Nur Dogan <>
2024-02-26 17:41:23 +0300

    Revert "no message"
    This reverts commit f3b48734c368c6ae7090445f7e37ad0c8d419dcb.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -52,12 +52,10 @@ const Footer = ({ t }: { t: TFunction }) => {
           <Row justify="space-between">
             <Col lg={10} md={10} sm={12} xs={12}>
               <Language>{t("Contact")}</Language>
-              <Large to="/">{t("Tell us everything")}</Large>
-              <Para>
-                {t(`Do you have any question? Feel free to reach out.`)}
-              </Para>
+              
+              
               <a href="mailto:tugcenur.dogan@gmail.com">
-                <Chat>{t(`Contact`)}</Chat>
+                <Chat><Language>{t("Contact")}</Language></Chat>
               </a>
             </Col>
           <Row justify="space-between">
